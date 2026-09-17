f27414851b2f31f109482f2160380cad4e78dfb9
Aryan-dev-geek <aryankashyap21r@gmail.com>
2026-09-17 17:09:02 +0530

    Update ingest_coingecko.py

--- src/bronze/ingest_coingecko.py
+++ src/bronze/ingest_coingecko.py
@@ -11,7 +11,7 @@ url = "https://api.coingecko.com/api/v3/coins/markets"
 params = {
     "vs_currency": "usd",
     "order": "market_cap_desc",
-    "per_page": 250,
+    "per_page": 150,
     "page": 1,
     "sparkline": "false"
 }
